5e154a0c74fdd66ad035f474dbfe459c3c98ac9c
Becca Smith <rsmith71@chicagobooth.edu>
2017-07-26 13:36:02 +0000

    835 am

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,4 +1,5 @@
 class PicturesController < ApplicationController
+  
   def new_form
     render("pic_templates/new_form.html.erb")
   end
@@ -7,40 +8,40 @@ class PicturesController < ApplicationController
     # The Parameters: {"the_source"=>"hi", "the_caption"=>"there"}
     
     p = Photo.new
-    p.source = params["the_source"]
-    p.caption = params["the_caption"]
+    p.source = params[:the_source]
+    p.caption = params[:the_caption]
     p.save
     
     @photo_count = Photo.count
-    render("pic_templates/create_row.html.erb")
+    rendirect_to("/photos")
   end
 
   def index
-    @list_of_photos = Photo.all
+    @list_of_photos = Photo.order(:created_at => :desc)
     render("pic_templates/index.html.erb")
   end
 
   def show
-    @the_id_number = params["the_id"]
-    @pic = Photo.find(params["the_id"])
+    @photo = Photo.find(params[:an_id])
     render("pic_templates/show.html.erb")
   end
 
   def edit_form
-    @photo_id = params["the_id"]
-    @p = Photo.find(params["the_id"])
-    @source = p.source
-    @caption = p.caption
+    @photo = Photo.find(params[:some_id])
     render("pic_templates/edit_form.html.erb")
   end
 
   def update_row
-    render("pic_templates/update_row.html.erb")
+    p = Photo.find(params[:the_id])
+    p.source = params[:the_source]
+    p.caption = params[:the_caption]
+    p.save
+    redirect_to("/photos/#{p.id}")
   end
 
   def destroy_row
-    @photos = Photo.find(params["the_id"])
-    @photos.destroy
-    render("pic_templates/destroy_row.html.erb")
+    p = Photo.find(params[:toast_id])
+    p.destroy
+    redirect_to("/photos")
   end
 end
